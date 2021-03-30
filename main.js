8bc7030fe65fc3d3ff2608c6d0894065d354e94e
Mariana Rodrigo <marianarodrigo90@gmail.com>
2021-03-30 13:17:13 -0300

    desafío1

---
+++ main.js
@@ -0,0 +1,7 @@
+
+let nombre = (prompt('Ingresa tu nombre'));
+let nacimiento = Number (prompt ('Ingresa tu año de nacimiento'));
+let resultado = Number (nacimiento - 1969);
+alert('Hola ' + nombre + "!");
+alert('Sabías que el año que naciste habían pasado ' + resultado + ' años desde la llegada del hombre a la Luna? Este es un dato irrelevante pero interesante');
+
