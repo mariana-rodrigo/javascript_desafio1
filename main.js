e4fd7438a9bd97dbb0e3f23f418508dfaff68d75
Mariana Rodrigo <marianarodrigo90@gmail.com>
2021-04-01 18:54:47 -0300

    desafio2

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-
-let nombre = (prompt('Ingresa tu nombre'));
-let nacimiento = Number (prompt ('Ingresa tu año de nacimiento'));
-let resultado = Number (nacimiento - 1969);
-alert('Hola ' + nombre + "!");
-alert('Sabías que el año que naciste habían pasado ' + resultado + ' años desde la llegada del hombre a la Luna? Este es un dato irrelevante pero interesante');
-
+let anioDeNacimiento = Number (prompt('Ingresá tu año de nacimiento'));
+let posteriorALaLlegada = anioDeNacimiento - 1969;
+let anteriorALaLlegada = 1969 - anioDeNacimiento;
+if (anioDeNacimiento > 1969){
+    alert ('Cuano naciste habían pasado ' + posteriorALaLlegada + " años de la llegada del Hombre a la Luna. Nuevamente, interesante pero irrelevante.")
+}
+else if (anioDeNacimiento === 1969){
+    alert ('Naciste el mismo año que el hombre llegó a la Luna')
+    let mesDeNacimiento = (prompt('Ingresá el mes que naciste'));
+    if (mesDeNacimiento === 'julio'){
+        alert ('Naciste el mismo mes que el hombre llegó a la Luna. Si no lo sabías, ahora lo sabés!');
+    }
+    else{
+        alert ('Pero no naciste en el mismo mes de la llegada del hombre a la Luna');
+    }
+}
+else{
+    alert ('Naciste '+anteriorALaLlegada+' años antes de la llegada del hombre a la Luna');
+}
