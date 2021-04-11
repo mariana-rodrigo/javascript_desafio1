d7dcdf9ec3d118f01df913e8179778ea48d7c66f
Mariana Rodrigo <marianarodrigo90@gmail.com>
2021-04-11 19:59:18 -0300

    desafío 3

--- main.js
+++ main.js
@@ -1,19 +1,6 @@
-let anioDeNacimiento = Number (prompt('Ingresá tu año de nacimiento'));
-let posteriorALaLlegada = anioDeNacimiento - 1969;
-let anteriorALaLlegada = 1969 - anioDeNacimiento;
-if (anioDeNacimiento > 1969){
-    alert ('Cuano naciste habían pasado ' + posteriorALaLlegada + " años de la llegada del Hombre a la Luna. Nuevamente, interesante pero irrelevante.")
-}
-else if (anioDeNacimiento === 1969){
-    alert ('Naciste el mismo año que el hombre llegó a la Luna')
-    let mesDeNacimiento = (prompt('Ingresá el mes que naciste'));
-    if (mesDeNacimiento === 'julio'){
-        alert ('Naciste el mismo mes que el hombre llegó a la Luna. Si no lo sabías, ahora lo sabés!');
-    }
-    else{
-        alert ('Pero no naciste en el mismo mes de la llegada del hombre a la Luna');
-    }
-}
-else{
-    alert ('Naciste '+anteriorALaLlegada+' años antes de la llegada del hombre a la Luna');
+let nombre = prompt ("Cuál es mi nombre?");
+
+while (nombre != "Mariana"){
+    alert ("Uy! Ingresaste "+nombre+" y es incorrecto, estamos buscando la respuesta Mariana. Tenés otra oportunidad");
+    nombre = prompt ("Ingresá otro nombre ;)");
 }
