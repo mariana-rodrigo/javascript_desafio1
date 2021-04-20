e92a4536ddd2db3fe4a46ee4da1508b56f28eebf
Mariana Rodrigo <marianarodrigo90@gmail.com>
2021-04-20 15:02:08 -0300

    desafío 5

--- main.js
+++ main.js
@@ -1,28 +1,43 @@
 let torta;
 let cantidad;
 
-function solicitarTortaYCantidad (){
-    torta =  prompt("Cuál de las siguientes tortas quieres ordenar: Lemon Pie - Cabsha - Rogel");
-    cantidad = Number( prompt ("De cuántos kilos es la torta que deseas ordenar?"));
+function Torta (nombre, descripcion, precio, recomendaciones){
+    this.nombre = nombre;
+    this.descripcion = descripcion;
+    this.precio = precio;
+    this.recomendaciones = recomendaciones;
 }
-let precio;
 
-function calcularTotal(cantidad, precio){
-    let total = cantidad * precio;
-    alert ("El total a pagar es "+ total);
+let tortaBomba = new Torta ("Torta Bomba", "Bizcochuelo húmedo de chocolate Merengue Italiano Mouse de chocolate", 1700, "Necesita refrigeración Se puede opción apta para celíacos y diabéticos");
+let tortaParaiso = new Torta ("Torta Paraíso", "Bizcochuelo de vainilla Relleno Ganache de chocolate con leche Recubierto con crocante de avellanas y maní", 1900, "No es necesario mantener refrigerada Se puede hacer opción apta para celíacos");
+let tortaInfierno = new Torta ("Torta Infierno", "Bizcochuelo Red Velvet Crema de Maracuyá y cardamomo Ganache de chocolate blanco", 1600, "Necesita refrigeración Se puede hacer opción apta para celíacos y veganos");
+let tortaLimaLimon = new Torta ("Torta Lima Limón", "Bizcochuelo de vainilla Relleno Ganache de limón con crocantes de pistacho Recubierto con merengue italiano con lima", 1500, "Necesario mantener refrigerada Se puede hacer opción apta para celíacos");
+
+function mostrarYSolicitarInformacion(tortaSeleccionada){
+    alert ("La descripción de la torta elegida es: "+ tortaSeleccionada.descripcion);
+    alert ("Las recomendaciones para la torta elegida son: "+ tortaSeleccionada.recomendaciones);
+    cantidad = Number( prompt ("De cuántos kilos es la torta que deseas ordenar?"));
+    let precioTotal = cantidad*tortaSeleccionada.precio;
+    alert ("El total a pagar es "+ precioTotal);
 }
-solicitarTortaYCantidad();
-if (torta ==="Lemon Pie"){
-    precio = 300;
-    calcularTotal(cantidad, precio);
-} else if ( torta === "Cabsha"){
-    precio = 200;
-    calcularTotal(cantidad, precio);
-} else if (torta === "Rogel"){
-    precio = 500;
-    calcularTotal(cantidad, precio);
-} else {
-    alert ("Opción incorrecta");
-    solicitarTortaYCantidad();
+
+function solicitarTorta (){
+    torta = prompt("Cuál de las siguientes tortas quieres ordenar: Torta Bomba - Torta Paraíso - Torta Infierno - Torta Lima Limón");
+    if (torta === tortaBomba.nombre){
+        mostrarYSolicitarInformacion(tortaBomba);
+    } else if ( torta === tortaParaiso.nombre){
+        mostrarYSolicitarInformacion(tortaParaiso);
+    } else if (torta === tortaInfierno.nombre){
+        mostrarYSolicitarInformacion(tortaInfierno);
+    } else if (torta === tortaLimaLimon.nombre){
+        mostrarYSolicitarInformacion(tortaLimaLimon);
+    } else {
+        alert ("Opción incorrecta");
+        solicitarTorta();
+    }
 }
+
+solicitarTorta();
+
+
 
